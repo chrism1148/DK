1fc8ef14dcc3fbda2af37a356396b38de3930723
chrism1148 <chrismccallan@hotmail.com>
2018-04-09 01:16:14 -0500

    Delete string_test.py

--- MLB/2018/string_test.py
+++
@@ -1,9 +0,0 @@
-import unidecode
-
-accented_characters = u'Málaga', u'é', u'á'
-# accented_string is of type 'unicode'
-
-unaccented_characters = unidecode.unidecode(accented_characters)
-
-
-print(unaccented_characters)
