f3b6e33edd48d92d6badafa24644154f4cea2e15
chrism1148 <chrismccallan@hotmail.com>
2018-04-22 15:08:42 -0500

    Update dk_mlb_projection_comparison_format.py

--- MLB/2018/dk_mlb_projection_comparison_format.py
+++ MLB/2018/dk_mlb_projection_comparison_format.py
@@ -11,7 +11,8 @@ game_date = 20180420
 ####################    DO NOT CHANGE BELOW THIS LINE  ###########################
 
 #path to file directory - HOME
-# formats = ('/Users/Chris/Desktop/Statis/MLB/2018/formats/')
+# lineup_path        = ('/Users/chris/downloads/')
+# projection_path    = ('/Users/chris/downloads/')
 
 #path to file directory - WORK
 downloads = '/Users/chrismccallan/Downloads'
