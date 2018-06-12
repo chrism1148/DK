a0ac40f6510d3ea298842548a73d1aa9767f84a3
chrism1148 <chrismccallan@hotmail.com>
2018-06-12 00:19:27 -0500

    Add files via upload

--- MLB/2018/gameday_format.py
+++ MLB/2018/gameday_format.py
@@ -9,26 +9,33 @@ import os
 import re
 
 
-game_date = 20180607
+game_date = 20180612
 
-
-#HOME
-downloads = '/Users/chris/Downloads'
-formats = ('/Users/chris/Downloads')
+# #HOME
+# downloads = '/Users/chris/Downloads'
+# formats = ('/Users/chris/Downloads')
 
 
 #WORK
-# downloads = '/Users/chrismccallan/Downloads'
-# formats = ('/Users/chrismccallan/Downloads')
+downloads = '/Users/chrismccallan/Downloads'
+formats = ('/Users/chrismccallan/Downloads')
 
 
 ifile  = os.path.join(downloads, 'gameday_raw_%d.csv' % (game_date))
 
-stats = pd.read_csv(ifile, encoding='ISO-8859-1')
+stats = pd.read_csv(ifile)
 stats.columns= ['player', 'team', 'opponent', 'position', 'salary']
 
 
 stats['player'] = stats['player'].str.strip()
+
+stats['home-away'] = stats['team'].str.replace(r'@[A-Z]{1,}', 'H')
+stats['home-away'] = stats['home-away'].str.replace(r'[A-Z]{2,3}', 'A')
+stats['home-away-opposite'] = stats['opponent'].str.replace(r'@[A-Z]{1,}', 'H')    #used to match the hitter's H/A to the pitchers H/A within BigQuery
+stats['home-away-opposite'] = stats['home-away-opposite'].str.replace(r'[A-Z]{2,3}', 'A')
+
+
+
 
 stats['team'] = stats['team'].str.replace('@', '')
 stats['team'] = stats['team'].str.lower()
@@ -42,5 +49,6 @@ stats['salary'] = stats['salary'].astype('int')
 
 stats['game_date'] = game_date
 stats['player hand'] = stats['player'].map(player_hand_lib.dict)
+
 
 stats.to_csv('gameday_formatted_%d.csv' % (game_date), index=0)
